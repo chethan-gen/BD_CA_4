1d9451de8287a33b6c25cfb7f740270681aa57fa
chethan-gen <chaithuregala123@gmail.com>
2025-07-02 10:48:49 +0530

    Delete backend/User1.js

--- backend/User1.js
+++
@@ -1,9 +0,0 @@
-class User{
-    constructor(username,hashedpassword,role){
-        this.username = username;
-        this.hashedpassword = hashedpassword;
-        this.role = role;
-    }
-}
-
-module.exports = User;
